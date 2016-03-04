bdebabcbb160b9fd84f06e890e611fee64a7f8e4
Gule Ifanger <valmir.ifanger@sensedia.com>
2016-03-04 15:45:15 -0300

    removing

--- compile.bat
+++
@@ -1,3 +0,0 @@
-SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
-%CSCPATH%\csc /reference:bin/Newtonsoft.Json.dll;bin/RestSharp.dll /target:library /out:bin/ima.Api.V1.Client.dll /recurse:src\*.cs /doc:bin/ima.Api.V1.Client.xml
-
